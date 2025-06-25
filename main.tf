adfcbaebfe31884824c84246504887360658d140
Eduardo Thums <eduardo.thums@cwi.com.br>
2025-06-25 00:57:12 -0300

    fix

--- main.tf
+++ main.tf
@@ -17,32 +17,28 @@ provider "helm" {
   # ]
 }
 
-resource "helm_release" "nginx_ingress" {
-  name       = "argo"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  create_namespace = true
-  namespace = "argocd"
 
-  # set = [
-  #   {
-  #     name  = "service.type"
-  #     value = "ClusterIP"
-  #   }
-  # ]
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
 }
 
-resource "helm_release" "cluster_boostrap" {
+resource "helm_release" "argocd" {
   name       = "argo"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   create_namespace = true
   namespace = "argocd"
+}
 
-  # set = [
-  #   {
-  #     name  = "service.type"
-  #     value = "ClusterIP"
-  #   }
-  # ]
+
+resource "kubectl_manifest" "test" {
+  yaml_body = file("./argocd/applicationSet.yaml")
+  depends_on = [ helm_release.argocd ]
 }
