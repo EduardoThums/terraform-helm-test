e6d75076e1ba516468b7b0497a4e1398200e2f3c
Eduardo Thums <eduardo.thums@cwi.com.br>
2025-06-25 21:40:06 -0300

    add girus-ingress helm

--- main.tf
+++ main.tf
@@ -38,7 +38,36 @@ resource "helm_release" "argocd" {
 }
 
 
-resource "kubectl_manifest" "test" {
+resource "kubectl_manifest" "cluster_boostrap" {
   yaml_body = file("./argocd/applicationSet.yaml")
   depends_on = [ helm_release.argocd ]
 }
+
+resource "null_resource" "wait_for_nginx" {
+  depends_on = [kubectl_manifest.cluster_boostrap]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Waiting for NGINX pods to be ready..."
+      kubectl wait --namespace ingress-nginx --for=create pod --selector=app.kubernetes.io/component=controller --timeout=60s
+      kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
+    EOT
+  }
+}
+
+resource "null_resource" "wait_for_cert_manager" {
+  depends_on = [kubectl_manifest.cluster_boostrap]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Waiting for NGINX pods to be ready..."
+      kubectl wait --namespace cert-manager pod --for=create -l app.kubernetes.io/instance=cert-manager --timeout 60s
+      kubectl wait --namespace cert-manager pod --for=condition=Ready -l app.kubernetes.io/instance=cert-manager --timeout 60s
+    EOT
+  }
+}
+
+# resource "kubectl_manifest" "test" {
+#   yaml_body = file("./argocd/applicationSet.yaml")
+#   depends_on = [ helm_release.argocd ]
+# }
